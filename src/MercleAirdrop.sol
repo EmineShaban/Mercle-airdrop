d599deeb66cef879903a3f5add34fb64d05891fa
Emine Shaban <emineshaban@icloud.com>
2025-03-21 11:51:51 +0200

    Project Setup

---
+++ src/MercleAirdrop.sol
@@ -0,0 +1,39 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+import {IERC20, SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import {MerkleProof} from "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
+
+contract MercleAirdrop{
+    using SafeERC20 for IERC20;
+
+    error MerkleProof__InvalidProof();
+    error MerkleProof__AlreadyClaimed();   
+
+    address[] claimers;
+
+    bytes32 private immutable i_merkleRoot;
+    IERC20 private immutable i_airdropToken;
+    mapping(address claimer => bool claimed) private s_hasClaimed;
+
+    event Claim(address account, uint256 amount);
+
+    constructor(bytes32 merkleRoot, IERC20 airdropToken){
+        i_merkleRoot = merkleRoot;
+        i_airdropToken = airdropToken;
+    }
+
+    function claim(address account, uint256 amount, bytes32[] calldata merkleProf) external{
+        if(s_hasClaimed[account]){
+            revert MerkleProof__AlreadyClaimed();
+        }
+
+        bytes32 leaf = keccak256(bytes.concat(keccak256(abi.encodePacked(account, amount))));
+        if(!MerkleProof.verify(merkleProf, i_merkleRoot, leaf)){
+            revert MerkleProof__InvalidProof();
+        }
+        s_hasClaimed[account] = true;
+        emit Claim(account, amount);
+        i_airdropToken.safeTransfer(account, amount);
+    }
+}
