43990153a3e9c398af31932d8aaf80be92e21c09
Emine Shaban <emineshaban@icloud.com>
2025-03-31 11:11:54 +0300

    .

--- src/MerkleAirdrop.sol
+++ src/MerkleAirdrop.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.24;
 import {IERC20, SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import {MerkleProof} from "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
 
-contract MercleAirdrop{
+contract MerkleAirdrop{
     using SafeERC20 for IERC20;
 
     error MerkleProof__InvalidProof();
